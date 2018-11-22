20338010b1dc3a6e96d6590b3750e7ffa0309216
Unknown <marek.kowalonek@gmail.com>
2018-11-22 22:44:45 +0100

    Display dishes from API

--- src/components/DishesList/index.js
+++ src/components/DishesList/index.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class DishesList extends Component {
+    state = {
+        dishes: [],
+    } 
+ 
+    componentDidMount(){
+        axios
+            .get('https://cookbook-koszalin.herokuapp.com/dishes')
+            .then(({ data }) => {
+                this.setState({ dishes: data })
+            })
+            .catch(error => {
+                console.log('error')
+            })
+    }
+    
     render(){
-        return <div>dishes</div>
+        const { dishes } = this.state
+        return (
+            <div>
+                <div>
+                    {dishes.map(i => <span key={i.id}>{i.name}</span>)}                
+                </div>
+            </div>
+        )
     }
 }
 
