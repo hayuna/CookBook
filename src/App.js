afeb7a86c83fae2fcb2282d17ae67687dcbbe769
Unknown <marek.kowalonek@gmail.com>
2018-11-23 00:01:13 +0100

    Create Dish Details View

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import DishesList from './components/DishesList'
+import DishDetails from './components/DishDetails'
 import NotFound from './components/utils/NotFound'
 import { Header } from './components/utils/Header'
 import { Container } from './style'
@@ -15,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={DishesList} />
             <Route exact path='/dishes' component={DishesList} />
+            <Route exact path='/dishes/:dishId' component={DishDetails} />
             <Route component={NotFound} />
           </Switch>
         </Container>
