ce6ad5876ba60faaa7409dc42ed9abfe6cfdefcc
Unknown <marek.kowalonek@gmail.com>
2018-11-28 21:46:23 +0100

    Restyle dish element on list

--- src/components/DishElement/style.js
+++ src/components/DishElement/style.js
@@ -2,9 +2,24 @@ import styled from 'styled-components'
 
 export const Thumb = styled.img`
     width:100px;
+    height:75px;
     border-radius: 45px;
 ` 
 
 export const Name = styled.span`
     color: white;
+    font-size: 2rem;
+    margin-left: 20px;
 ` 
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    & > a {
+        display: flex;
+        flex-direction: row;
+        padding: 5px;
+        align-items: center;
+        text-decoration: none;
+    }
+`
