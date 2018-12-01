a6af9f9b923557808dd055138b714be099d58c6b
Unknown <marek.kowalonek@gmail.com>
2018-12-01 22:16:15 +0100

    Add Dish Name Component

---
+++ src/components/AddNewDish/DishName/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    margin-left: 20px
+`
+
+export const Label = styled.label`
+    width: 30vw;
+`
+
+export const Input = styled.input`
+    color: white;
+`
