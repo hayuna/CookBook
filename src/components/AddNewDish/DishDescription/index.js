d16e59ea84b226822d06318ac448d9294bf8cf42
Unknown <marek.kowalonek@gmail.com>
2018-12-01 22:16:56 +0100

    Add Dish Description Component

---
+++ src/components/AddNewDish/DishDescription/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Container, TextArea, Label } from './style'
+
+class DishDescription extends Component{
+
+    state = {
+        value: ''
+    }
+
+    handleChange = e => {
+        this.setState({value: e.target.value})
+        this.props.onChangeValue(e.target.value)
+    }
+
+    render(){
+        return(
+            <Container>
+                <Label>Description
+                    <div>
+                        <TextArea value={this.state.value} onChange={this.handleChange} />
+                    </div>
+                </Label>
+            </Container>
+        )
+    }
+}
+
+export default DishDescription
---
+++ src/components/AddNewDish/DishDescription/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    margin-top:10px;
+`
+
+export const TextArea = styled.textarea`
+    width: 100%;
+    height: 300px;
+    border-radius: 14px;
+    resize: none;
+`
+
+export const Label = styled.label`
+    color: white;
+`
