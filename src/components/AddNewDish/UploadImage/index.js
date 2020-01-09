1a673467a72fdc91dd7ee8a14e1cc76a0d585012
mras2303 <marek.kowalonek@gmail.com>
2020-01-09 21:22:27 +0100

    Update index.js

--- src/components/AddNewDish/UploadImage/index.js
+++ src/components/AddNewDish/UploadImage/index.js
@@ -1,55 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './style.css'
 
-class UploadImage extends Component {
-  state = {
-    active: false,
-    imageSrc: '',
-    loaded: false
-  }
+const UploadImage = ({ onFileChosen, activeColor, baseColor, overlayColor }) => {
+    const [active, setActive] = useState(false)
+    const [imageSrc, setImageSrc] = useState('')
+    const [loaded, setLoaded] = useState(false)
   
-  onDrop = e => {
-    e.preventDefault();
-    this.setState({ active: false });
-    this.onFileChange(e, e.dataTransfer.files[0]);
-  }
-  onFileChange = (e, file) => {
-    file = file || e.target.files[0]
-    const reader = new FileReader();
-    
-    if (!file)return;
-    else if(!file.type.match(/image-*/))return
-    
-    this.setState({ loaded: false });
-    reader.onload = e => {
-        this.setState({ 
-            imageSrc: reader.result, 
-            loaded: true 
-        }); 
-        this.props.onFileChosen(reader.result)
+    const onDrop = e => {
+        e.preventDefault();
+        setActive(false)
+        onFileChange(e, e.dataTransfer.files[0]);
     }
+  
+    const onFileChange = (e, file) => {
+        file = file || e.target.files[0]
+        const reader = new FileReader();
     
-    reader.readAsDataURL(file);
-  }
+        if (!file) return;
+        else if(!file.type.match(/image-*/)) return
+        setLoaded(false)
+        reader.onload = e => {
+            setImageSrc(reader.result)
+            setLoaded(true)
+            onFileChosen(reader.result)
+        }
+        reader.readAsDataURL(file);
+    }
 
-
-
-  render() {
-    const { loaded, active, imageSrc } = this.state
-    const { activeColor, baseColor, overlayColor } = this.props
     const iconColor = active ? activeColor : loaded ? overlayColor : baseColor;
+    
     return (
-      <label 
-        className={`uploader ${loaded ? 'loaded' : ''}`}
-        onDrop={this.onDrop}
-        style={{outlineColor: active ? activeColor : baseColor, marginLeft: '8px'}}>
-        
-        <img alt='temp' src={imageSrc} className={loaded ? 'loaded' : ''}/>
-        <i className="icon icon-upload" style={{ color: iconColor }}></i>
-        <input type="file" accept="image/*" onChange={this.onFileChange} ref="input" />
-      </label>
+        <label 
+            className={`uploader ${loaded ? 'loaded' : ''}`}
+            onDrop={onDrop}
+            style={{outlineColor: active ? activeColor : baseColor, marginLeft: '8px'}}>
+            <img alt='temp' src={imageSrc} className={loaded ? 'loaded' : ''}/>
+            <i className="icon icon-upload" style={{ color: iconColor }}></i>
+            <input type="file" accept="image/*" onChange={onFileChange} ref="input" />
+        </label>
     )
-  }
 }
 
 export default UploadImage;
