04d923a37c4399c047cdba6343927829a19c74bc
mras2303 <marek.kowalonek@gmail.com>
2020-01-09 21:18:03 +0100

    Update index.js

--- src/components/AddNewDish/index.js
+++ src/components/AddNewDish/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 import { Redirect } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
@@ -12,22 +12,19 @@ import FloatingButton from '../utils/FloatingButton';
 import { API_GET_DISHES } from '../../api';
 import { UPLOAD_DISH_PHOTO, TYPE_DISH_NAME, TYPE_DISH_DESCRIPTION, CHOOSE_SOME_INGREDIENTS_ALERT, ADDED_NEW_DISH } from '../../texts'
 
-class AddNewDish extends Component{
-
-    state = {
-        image: '',
-        name: '',
-        description: '',
-        ingredients: [],
-        added: false,
-    }
+const AddNewDish = () => {
+    const [image, setImage] = useState('')
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    const [added, setAdded] = useState(false)
     
-    handleFileChosen = image => this.setState({ image })
-    handleName = name => this.setState({ name })
-    handleDescription = description => this.setState({ description })
-    handleIngredientChosen = ingredients => this.setState({ ingredients })
-    addDish = () => {
-        const errors = this.validate()
+    const handleFileChosen = image => setImage(image)
+    const handleName = name => setName(name)
+    const handleDescription = description => setDescription(description)
+    const handleIngredientChosen = ingredients => setIngredients(ingredients)
+    const addDish = async () => {
+        const errors = validate()
         if(errors.length !== 0){
             errors.map(error => {
                 toast.error(error, {
@@ -35,29 +32,25 @@ class AddNewDish extends Component{
                 })
                 return null;
             })            
-        }else{
-            const { name, description, image, ingredients } = this.state
-            axios
-            .post(API_GET_DISHES, {
-                "name": name,
-                "picture": image,
-                "recipe": description,
-                "ingredientIds": ingredients
-            })
-            .then(() => {
-                toast.success(ADDED_NEW_DISH, {
-                    onClose: () => this.setState({ added: true })
+        } else {
+            try {
+                await axios.post(API_GET_DISHES, {
+                    name,
+                    picture: image,
+                    recipe: description,
+                    ingredientIds: ingredients
                 })
-            })
-            .catch(error => {
+                toast.success(ADDED_NEW_DISH, {
+                    onClose: () => setAdded(true)
+                })
+            } catch(error) {
                 toast.error(error.message)
-            })
+            }
         }
     }
 
-    validate = () => {
-        const { image, name, description, ingredients } = this.state
-        let listOfErrors = []
+    const validate = () => {
+        const listOfErrors = []
         if(image.length === 0) listOfErrors.push(UPLOAD_DISH_PHOTO)
         if(name.length === 0) listOfErrors.push(TYPE_DISH_NAME)
         if(description.length === 0) listOfErrors.push(TYPE_DISH_DESCRIPTION)
@@ -65,23 +58,20 @@ class AddNewDish extends Component{
         return listOfErrors
     }
 
-    render(){
-        const { added } = this.state
-        if(added) return <Redirect to='/dishes' />
-        return(
-            <div>
-                <ToastContainer autoClose={1000} position={toast.POSITION.TOP_CENTER} />
-                <Header redirect />
-                <div className="upperContainer" style={{marginTop: '20px'}}>
-                    <UploadImage onFileChosen={ this.handleFileChosen } />
-                    <DishName onChangeValue={ this.handleName } />
-                </div>
-                <IngredientsSelect onChangeValue={ this.handleIngredientChosen } />
-                <DishDescription onChangeValue={ this.handleDescription } />
-                <FloatingButton icon='tick' onClick={this.addDish} />
+    return(
+        <div>
+            { added && <Redirect to='/dishes' /> }
+            <ToastContainer autoClose={1000} position={toast.POSITION.TOP_CENTER} />
+            <Header redirect />
+            <div className="upperContainer" style={{marginTop: '20px'}}>
+                <UploadImage onFileChosen={ handleFileChosen } />
+                <DishName onChangeValue={ handleName } />
             </div>
-        )
-    }
+            <IngredientsSelect onChangeValue={ handleIngredientChosen } />
+            <DishDescription onChangeValue={ handleDescription } />
+            <FloatingButton icon='tick' onClick={addDish} />
+        </div>
+    )
 }
 
 export default AddNewDish
