b5b20959c6533e850d3e8d193dd9f40810e90b8c
mras2303 <marek.kowalonek@gmail.com>
2020-01-09 20:46:06 +0100

    Update index.js

--- src/components/utils/NotFound/index.js
+++ src/components/utils/NotFound/index.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Redirect } from 'react-router-dom'
 import { ADDRESS_NOT_FOUND, NOT_FOUND_REDIRECTING, SECONDS } from '../../../texts';
 
-class NotFound extends Component {
-    state = {
-        counter: 10
+const NotFound = ({ location }) => {
+    const [counter, setCounter] = useState(10)
+    const [intervalId, setIntervalId] = useState(null)
+
+    useEffect(
+        () => {
+            const intervalId = setInterval(countdown, 1000)
+            setIntervalId(intervalId)
+            return () => clearInterval(intervalId)        
+        }, [intervalId]
+    )
+
+    const countdown = () => {
+        setCounter(counter - 1)
     }
 
-    componentDidMount = () => {
-        const intervalId = setInterval(this.countdown, 1000)
-        this.setState({intervalId})
-    }
-
-    countdown = () => {
-        this.setState({counter: this.state.counter - 1})
-    }
-
-    componentWillUnmount = () => clearInterval(this.state.intervalId)
-
-    render(){
-        const { location } = this.props
-        const { counter } = this.state
-        return (
-            <div>
-                <p>{ADDRESS_NOT_FOUND} <code>{ location.pathname }</code></p>
-                <p>{NOT_FOUND_REDIRECTING} { counter } {SECONDS}</p>
-                {counter === 0 && <Redirect to='/' />}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <p>{ADDRESS_NOT_FOUND} <code>{ location.pathname }</code></p>
+            <p>{NOT_FOUND_REDIRECTING} { counter } {SECONDS}</p>
+            {counter === 0 && <Redirect to='/' />}
+        </div>
+    )
 }
 
 export default NotFound
