04f572e9e19f3687bbe88b4f9ae6de1dfa03c383
mras2303 <marek.kowalonek@gmail.com>
2020-01-09 21:32:05 +0100

    Update index.js

--- src/components/AddNewDish/IngredientsSelect/index.js
+++ src/components/AddNewDish/IngredientsSelect/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -14,92 +14,88 @@ import { API_GET_INGREDIENTS } from '../../../api';
 import { Container, FullContainer, Label } from './style'
 import { ADD_NEW_INGREDIENT, NAME, CHOOSE_SOME_INGREDIENTS, ADD, CANCEL, CHOOSE_INGREDIENT_PLACEHOLDER } from '../../../texts';
 
-class IngredientsSelect extends Component{
-    state = {
-        selectedOption: null,
-        options: [],
-        open: false,
-        name: ''
-    }
+const IngredientsSelect = ({ onChangeValue }) => {
+    const [selectedOption, setSelectedOption] = useState(null)
+    const [options, setOptions] = useState([])
+    const [open, setOpen] = useState(false)
+    const [name, setName] = useState('')
 
-    handleClickOpen = () => this.setState({ open: true })
-    handleClose = () => this.setState({ open: false })
-    changeIngredient = e => this.setState({ name: e.target.value })
-    addNewIngredient = () => {
-        this.setState({ open: false });
-        const { name, options, selectedOption } = this.state
-        axios
-        .post(API_GET_INGREDIENTS, {name})
-        .then(({ data }) => {
-            data.value = data.id; 
-            data.label = data.name
-            this.setState({ options: options ? [...options, data] : data })
-            this.setState({ selectedOption: selectedOption ? [ ...selectedOption, data ] : data })
-        })
-        .catch(() => {
+    const handleClickOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+    const changeIngredient = e => setName(e.target.value)
+    const addNewIngredient = async () => {
+        try{
+            handleClose()
+            const result = await axios.post(API_GET_INGREDIENTS, {name})
+            result.data.value = result.data.id; 
+            result.data.label = result.data.name
+            setOptions(options ? [...options, data] : data)
+            setSelectedOption(selectedOption ? [ ...selectedOption, data ] : data)    
+        }catch {
             const usedIngredient = options.filter(option => option.name === name)
-            this.setState({ options: options ? [...options, usedIngredient] : usedIngredient })
-            this.setState({ selectedOption: selectedOption ? [ ...selectedOption, usedIngredient ] : usedIngredient })
-        })
-        .then(() => {
-            const ingredients = this.state.selectedOption.map(option => option.value)
-            this.props.onChangeValue(ingredients)
-        })
-    }
-    
-    componentDidMount(){
-        axios
-        .get(API_GET_INGREDIENTS)
-        .then(({ data }) => {
-            const options = data.map(d => { d.value = d.id; d.label = d.name; return d })
-            this.setState({ options })
-        })
-        .catch(error => {
-            toast.error(error.message)
-        })
+            setOptions(options ? [...options, usedIngredient] : usedIngredient)
+            setSelectedOption(selectedOption ? [ ...selectedOption, usedIngredient ] : usedIngredient)
+        } finally {
+            const ingredients = selectedOption.map(option => option.value)
+            onChangeValue(ingredients)
+        }
     }
 
-    handleChange = selectedOption => {
-        this.setState({ selectedOption });
-        const ingredients = selectedOption.map(option => option.value)
-        this.props.onChangeValue(ingredients)
+    const getIngredients = async () => {
+        try{
+            const result = await axios.get(API_GET_INGREDIENTS)
+            const opts = result.data.map(d => { d.value = d.id; d.label = d.name; return d })
+            setOptions(opts)    
+        } catch(error){
+            toast.error(error.message)
+
+        }
+    }
+
+    useEffect(
+        () => {
+            getIngredients()
+        }, []
+    )
+
+    const handleChange = selected => {
+        setSelectedOption(selected)
+        const ingredients = selected.map(option => option.value)
+        onChangeValue(ingredients)
     }
     
-    render(){
-        const { selectedOption, options, open } = this.state;
-        return (
-            <FullContainer>
-                <ToastContainer autoClose={1000} position={toast.POSITION.TOP_CENTER} />
-                <Label>{CHOOSE_SOME_INGREDIENTS}</Label>
-                <Container>
-                    <Select 
-                        className='ingredientSelect'
-                        clearableValue={false} 
-                        clearable={false} 
-                        backspaceRemovesValue={false} 
-                        placeholder={CHOOSE_INGREDIENT_PLACEHOLDER} 
-                        value={selectedOption} 
-                        onChange={this.handleChange} 
-                        options={options} 
-                        isMulti
-                    />
-                    <div style={{marginLeft: '10px'}}>
-                        <FloatingButton icon='add' onClick={this.handleClickOpen} />
-                        <Dialog open={open} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-                            <DialogTitle id="form-dialog-title">{ADD_NEW_INGREDIENT}</DialogTitle>
-                            <DialogContent>
-                                <TextField onChange={this.changeIngredient} autoFocus margin="dense" id="name" label={NAME} type="text" fullWidth/>
-                            </DialogContent>
-                            <DialogActions>
-                                <Button onClick={this.handleClose} color="primary">{CANCEL}</Button>
-                                <Button onClick={this.addNewIngredient} color="primary">{ADD}</Button>
-                            </DialogActions>
-                        </Dialog>
-                    </div>
-                </Container>
-            </FullContainer>
-        )
-    }
+    return (
+        <FullContainer>
+            <ToastContainer autoClose={1000} position={toast.POSITION.TOP_CENTER} />
+            <Label>{CHOOSE_SOME_INGREDIENTS}</Label>
+            <Container>
+                <Select 
+                    className='ingredientSelect'
+                    clearableValue={false} 
+                    clearable={false} 
+                    backspaceRemovesValue={false} 
+                    placeholder={CHOOSE_INGREDIENT_PLACEHOLDER} 
+                    value={selectedOption} 
+                    onChange={handleChange} 
+                    options={options} 
+                    isMulti
+                />
+                <div style={{marginLeft: '10px'}}>
+                    <FloatingButton icon='add' onClick={this.handleClickOpen} />
+                    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                        <DialogTitle id="form-dialog-title">{ADD_NEW_INGREDIENT}</DialogTitle>
+                        <DialogContent>
+                            <TextField onChange={changeIngredient} autoFocus margin="dense" id="name" label={NAME} type="text" fullWidth/>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">{CANCEL}</Button>
+                            <Button onClick={addNewIngredient} color="primary">{ADD}</Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
+            </Container>
+        </FullContainer>
+    )
 }
 
 export default IngredientsSelect
